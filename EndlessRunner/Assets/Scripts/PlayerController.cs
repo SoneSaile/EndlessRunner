e8e7f9a7e4057fbcf2be60ef1646bfbf31a60453
Sone Saile <enoselias28@gmail.com>
2020-03-10 22:26:18 -0300

    Segundo Dia

---
+++ EndlessRunner/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+    private Rigidbody2D rigidbody;
+    public bool onFloor;
+    public LayerMask groundMask;
+    private Collider2D collider;
+    private Animator animatorPlayer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
+        animatorPlayer = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        onFloor = Physics2D.IsTouchingLayers(collider, groundMask);
+        rigidbody.velocity = new Vector2(moveSpeed, rigidbody.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Space) && onFloor)
+        {
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpForce);
+
+        }
+        animatorPlayer.SetFloat("MoveSpeed", rigidbody.velocity.x);
+        animatorPlayer.SetBool("onFloor", onFloor);
+    }
+}
